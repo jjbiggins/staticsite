3e69028a53e5b2bbdd93ba3b4812822f43ab8696
Joe Biggins <joe+github@biggins.tech>
2021-07-18 20:39:27 -0500

    static site code

---
+++ config.ts
@@ -0,0 +1,168 @@
+const siteMetadata = {
+    title: `Joe Biggins`,
+    siteUrl: `http://localhost`,
+    capitalizeTitleOnHome: false,
+    logo: `/images/logo.png`,
+    icon: `/images/icon.png`,
+    titleImage: `/images/mandlebrot.jpg`,
+    ogImage: `/images/mandlebrot.jpg`,
+    twoColumnWall: true,
+    cookiePolicy: true,
+    introTag: `Developer | Designer | Artist`,
+    description: `Software Developer by profession. Software Explorer by passion`,
+    about:
+        "I'm a pretty simple guy that finds beauty in the simple things. Here's my portfolio; I needed to bolster my web dev chop to round out a few personal projects; this is those reps, but if it helps with jobs, that's great too",
+    author: `@_akzhy`,
+    blogItemsPerPage: 10,
+    portfolioItemsPerPage: 10,
+    darkmode: true,
+    switchTheme: true,
+    navLinks: [
+        {
+            name: "HOME",
+            url: "/",
+        },
+        {
+            name: "ABOUT",
+            url: "/about",
+        },
+        {
+            name: "BLOG",
+            url: "/blog",
+        },
+        {
+            name: "PORTFOLIO",
+            url: "/portfolio",
+        },
+        {
+            name: "CONTACT",
+            url: "/contact",
+        },
+    ],
+    footerLinks: [
+        {
+            name: "PRIVACY POLICY",
+            url: "/privacy-policy",
+        },
+        {
+            name: "GitHub",
+            url: "https://github.com/jjbiggins",
+        },
+    ],
+    social: [
+        {
+            name: "Facebook",
+            icon: "/images/Facebook.svg",
+            url: "#",
+        },
+        {
+            name: "Twitter",
+            icon: "/images/Twitter.svg",
+            url: "#",
+        },
+        {
+            name: "Instagram",
+            icon: "/images/Instagram.svg",
+            url: "#",
+        },
+        {
+            name: "Youtube",
+            icon: "/images/Youtube.svg",
+            url: "#",
+        },
+    ],
+    contact: {
+        // leave empty ('') or false to hide form
+        api_url: "https://getform.io/f/f227a36e-096a-4c6a-9963-9f1918a85bb3",
+        description: `Give me ring. I'm a pretty friendly guy. Though, admittedly, not great at answering my phone`,
+        mail: "joe@biggins.tech",
+        phone: "(630) 842-0626",
+        address: "306 Arlington Ave",
+    },
+    disqus: "elemental-netlify-com",
+}
+
+const beforeContactFormSubmit = data => {
+    // Code 0 - success
+    // Code 1 - Name
+    // Code 2 - Email
+    // Code 3 - Message
+    // Code 4 - Other
+    const errors = []
+
+    if (data.name.trim().length < 2) {
+        errors.push({
+            code: 1,
+            message: "Enter a name",
+        })
+    }
+
+    if (!data.email.match(/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/)) {
+        errors.push({
+            code: 2,
+            message: "Enter a valid email address",
+        })
+    }
+
+    if (data.message.trim().length < 15) {
+        errors.push({
+            code: 3,
+            message: "Enter a message with atleast 15 characters",
+        })
+    }
+
+    if (errors.length > 0)
+        return {
+            result: false,
+            errors: errors,
+        }
+
+    return {
+        data: {
+            name: data.name,
+            email: data.email,
+            message: data.message,
+        },
+        result: true,
+    }
+}
+
+const contactFormSubmit = async (api, data) => {
+    let res: any = await fetch(api, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+    })
+
+    res = await res.json()
+
+    if (res.success) {
+        return {
+            result: true,
+        }
+    }
+    return {
+        result: false,
+        ...res,
+    }
+}
+
+const defaults = {
+    disqus: null,
+    twoColumnWall: true,
+    darkmode: false,
+    switchTheme: true,
+    capitalizeTitleOnHome: true,
+    cookiePolicy: false
+}
+
+Object.keys(defaults).forEach(item => {
+    if (siteMetadata[item] === undefined) {
+        siteMetadata[item] = defaults[item]
+    }
+})
+
+export { siteMetadata, beforeContactFormSubmit, contactFormSubmit }
